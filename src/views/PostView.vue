72d0e603d306607a826dff17e35399c28c8320e6
Hiparco Evenor Martínez López <hemartinez@siboif.gob.ni>
2025-05-01 12:18:46 -0600

    Inicializacion proyecto vue 3, router.

---
+++ src/views/PostView.vue
@@ -0,0 +1,17 @@
+<template>
+ <h1>This a post {{ postId }} page</h1>
+
+</template>
+
+<script setup lang="ts">
+import { onMounted, ref } from 'vue';
+import { useRoute } from 'vue-router';
+
+let postId = ref('');
+
+onMounted(() => {
+  const route = useRoute();
+  postId.value = route.params.id.toString();
+});
+
+</script>
